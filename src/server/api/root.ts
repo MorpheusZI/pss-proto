7c8b33636655fc39c6ba27f076f2063f65838c40
ahmad aby ayyasi <abiayyasi12@gmail.com>
2024-05-24 13:01:24 +0700

    testing trpc

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { lookupUser } from "./routers/lookup";
 
 /**
  * This is the primary router for your server.
@@ -7,6 +8,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  checkUser: lookupUser,
   post: postRouter,
 });
 
---
+++ src/server/api/routers/lookup.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const schema = z.object({
+  email: z.string().email({ message: "Invalid email" }),
+  password: z
+    .string()
+    .min(5, { message: "Password should have at least 5 letters" }),
+});
+
+export const lookupUser = createTRPCRouter({
+  check: publicProcedure
+    .input(schema)
+    .mutation(({ input }) => {
+      return `Hello ${input.email} and ${input.password}`;
+    }),
+});
