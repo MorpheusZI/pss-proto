2424bb32041b6a28d7b08995542be39ed04dbc4b
Morpheus Zein <morpheus.zein@irawan.id>
2024-05-31 18:28:47 +0700

    fixing build errors

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,12 +1,31 @@
 "use client"
-import { Flex } from "@mantine/core";
+import { Flex, rem } from "@mantine/core";
 import Sidebar from "../_components/Home/Sidebar";
 import Main from "../_components/Home/Main";
 import { readSessionStorageValue } from "@mantine/hooks";
 import { CurrentUser } from "~/types/types";
+import { useRouter } from "next/navigation";
+import { notifications } from "@mantine/notifications";
+import { IconLockX } from "@tabler/icons-react"
 
 export default function Home() {
+  const router = useRouter()
   const CurrentUser = readSessionStorageValue<CurrentUser>({ key: "CurrentUser" })
+  const IconNope = <IconLockX style={{ width: rem(18), height: rem(18) }} />
+  if (!CurrentUser) {
+    notifications.show({
+      title: "Anda belum login!",
+      message: "Mohon login terlebih dahulu",
+      icon: IconNope,
+      color: "red",
+      bg: "dark",
+      styles: () => ({
+        title: { color: "white", fontWeight: "bold" },
+        description: { color: "white" }
+      })
+    })
+    return router.push("/Login")
+  }
 
   return <Flex>
     <Sidebar />
